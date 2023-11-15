2c35964c5d2b8143f88976ebeb2d85b9c68df97f
root <root@localhost.localdomain>
2023-11-15 22:56:37 +0100

    update

--- chdb/query.py
+++ chdb/query.py
@@ -4,11 +4,15 @@ import chdb
 import timeit
 import sys
 
+from chdb import session as chs
+db = chs.Session()
+
+
 query = sys.stdin.read()
 print(query)
 
 for try_num in range(3):
     start = timeit.default_timer()
-    chdb.query(query, "Null")
+    db.query(query, "Null")
     end = timeit.default_timer()
     print(end - start)
